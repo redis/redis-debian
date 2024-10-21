28c95e531cd9a8f4726ebfabc5b1dd74e9e9bedb
maxb-io <105273783+maxb-io@users.noreply.github.com>
2024-10-21 11:34:15 +0300

    RED-135059 add support for arm64 arch (#9)
    * added architecture to matrix for arm64

--- setup_sbuild.sh
+++ setup_sbuild.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 if [ $# != 2 ]; then
     echo "Please use: setup_build.sh [dist] [arch]"
@@ -13,11 +12,15 @@ else
     disttype="debian"
 fi
 
-# Determine base apt repository URL based on type of distribution.
+# Determine base apt repository URL based on type of distribution and architecture.
 case "$disttype" in
     ubuntu)
-        url=http://archive.ubuntu.com/ubuntu
-        ;;
+        if [ "$arch" = "arm64" ]; then
+            url=http://ports.ubuntu.com/ubuntu-ports
+        else
+            url=http://archive.ubuntu.com/ubuntu
+        fi
+         ;;
     debian)
         url=http://deb.debian.org/debian
         ;;
@@ -31,12 +34,29 @@ sbuild-createchroot \
     ${dist} `mktemp -d` ${url}
 
 # Ubuntu has the main and ports repositories on different URLs, so we need to
-# properly set up /etc/apt/sources.list to make cross compilation work.
+# properly set up /etc/apt/sources.list to make cross compilation work
+# and enable multi-architecture support inside a chroot environment
 if [ "$disttype" = "ubuntu" ]; then
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture i386
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture armhf
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture arm64
+    # Update /etc/apt/sources.list for cross-compilation (Ubuntu)
     cat <<__END__ | schroot -c source:${dist}-${arch}-sbuild -d / -- tee /etc/apt/sources.list
 deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${dist} main universe
 deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${dist}-updates main universe
 deb [arch=armhf,arm64] http://ports.ubuntu.com ${dist} main universe
 deb [arch=armhf,arm64] http://ports.ubuntu.com ${dist}-updates main universe
+__END__
+
+elif [ "$disttype" = "debian" ]; then
+#   enable multi-architecture support inside a chroot environment
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture i386
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture armhf
+    schroot -c source:${dist}-${arch}-sbuild -d / -- dpkg --add-architecture arm64
+    # Update /etc/apt/sources.list for cross-compilation  (Debian)
+    cat <<__END__ | schroot -c source:${dist}-${arch}-sbuild -d / -- tee /etc/apt/sources.list
+deb [arch=amd64,i386,armhf,arm64] http://deb.debian.org/debian ${dist} main contrib non-free non-free-firmware
+deb [arch=amd64,i386,armhf,arm64] http://deb.debian.org/debian ${dist}-updates main contrib non-free non-free-firmware
+deb [arch=amd64,i386,armhf,arm64] http://deb.debian.org/debian-security ${dist}-security main contrib non-free non-free-firmware
 __END__
 fi
