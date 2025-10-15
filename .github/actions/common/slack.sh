5f8aa6e7e57bf3b383502c7e7b63ac20df48c185
Petar Shtuchkin <petar.shtuchkin@redis.com>
2025-10-15 21:08:23 +0300

    Fix slack message expression

--- .github/actions/common/slack.sh
+++ .github/actions/common/slack.sh
@@ -5,7 +5,7 @@ slack_format_success_message() {
 jq --arg release_tag "$1" --arg url_prefix "$1" --arg footer "$3" --arg env "$4" '
 {
   icon_emoji: ":redis-circle:",
-  text: (":debian: Debian Packages Published for Redis: " + $release_tag + " (" + $env + ")")),
+  text: (":debian: Debian Packages Published for Redis: " + $release_tag + " (" + $env + ")"),
   blocks: [
     {
       "type": "header",
@@ -20,14 +20,14 @@ jq --arg release_tag "$1" --arg url_prefix "$1" --arg footer "$3" --arg env "$4"
           (
             to_entries
             | map(
-                "Distribution: *" + .key + "*\n" +
+                . as $dist_entry | "Distribution: *" + .key + "*\n" +
                 (
                   .value | to_entries
                   | map(
                       "  Architecture: *" + .key + "*\n" +
                       (
                         .value
-                        | map("    • <" + $url_prefix + "/" + $dist + "/r/re/" + . + "|" + . + ">")
+                        | map("    • <" + $url_prefix + "/" + $dist_entry.key + "/r/re/" + . + "|" + . + ">")
                         | join("\n")
                       )
                     )
